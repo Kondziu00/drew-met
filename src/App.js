d38b603792c674c7427dc058066acf283b636430
Konrad <konrad@admins-MacBook-Air.local>
2025-11-19 11:42:18 +0100

    Added .js and .css AboutUs file and added to App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Footer from './components/Footer/Footer';
 import Services from './components/MainPage/Services';
 import Contact from './components/MainPage/Contact';
 import Gallery from './components/MainPage/Gallery';
+import AboutUs from './components/MainPage/AboutUs';
 
 const App = () => {
 	return (
 		<Router>
 			<Nav />
-		<main>
-			<Header />
-			<Services />
-			<Gallery />
-			<Contact/>
-		</main>
+			<main>
+				<Header />
+				<Services />
+				<AboutUs />
+				<Gallery />
+				<Contact />
+			</main>
 			<Footer />
 		</Router>
 	);
---
+++ src/components/MainPage/AboutUs.js
@@ -0,0 +1,35 @@
+import './AboutUs.css';
+
+const AboutUs = () => {
+	return (
+		<section className='AboutUs'>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+		</section>
+	);
+};
+
+export default AboutUs;
