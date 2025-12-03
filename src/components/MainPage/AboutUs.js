bc593cc57af01986cfcb1f4e176a85183b202583
Konrad <konrad.zgryzniak1@interia.pl>
2025-12-03 21:04:07 +0100

    Added section aboutus

--- src/components/MainPage/AboutUs.js
+++ src/components/MainPage/AboutUs.js
@@ -1,33 +1,39 @@
 import './AboutUs.css';
+import about from '../../images/about.jpg';
 
 const AboutUs = () => {
 	return (
 		<section id='about' className='AboutUs'>
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
+			<div className='upper-about'>
+				<div className='about-image'>
+					<img src={about} alt='Zdjęcie dostawy drewna' className='about' />
+				</div>
+				<div className='aboutusinfo'>
+					<h2 className='aboutustitle'>Poznaj Nas</h2>
+					<h3 className='aboutustext'>
+						<p>
+							<span>
+								Tradycja, jakość i solidność w obróbce drewna od wielu lat.
+							</span>
+							<br />
+							<span>
+								Drew-Met to rodzinna firma, która od wielu lat dostarcza drewno najwyższej jakości wraz z realizacją większych projektów.
+							</span>
+							<br />
+							<br />
+							<span>
+								Nasza pasja do drewna i zaangażowanie w każdy projekt sprawiają, że jesteśmy zaufanym partnerem dla naszych klientów.
+							</span>
+							<br />
+							<br />
+							<br />
+							<span>
+								Zapraszamy do współpracy!
+							</span>
+						</p>
+					</h3>
+				</div>
+			</div>
 		</section>
 	);
 };
