52357972f8ce8fc853a1f78db240631ce0ffdd5a
Konrad <konrad@admins-Air.localdomain>
2026-01-28 08:22:23 +0100

    First images in gallery

--- src/components/MainPage/Gallery.js
+++ src/components/MainPage/Gallery.js
@@ -1,35 +1,23 @@
 import './Gallery.css';
 
+import img1 from '../../images/about.jpg';
+import img2 from '../../images/about.jpg';
+import img3 from '../../images/about.jpg';
+
+const images = [img1, img2, img3];
+
 const Gallery = () => {
 	return (
 		<section id='gallery' className='gallery'>
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
-			<br />
+			<h2 className='gallery__title'>Galeria</h2>
+
+			<div className='gallery__flex'>
+				{images.map((src, index) => (
+					<div className='gallery__item' key={index}>
+						<img src={src} alt={`Galeria ${index + 1}`} />
+					</div>
+				))}
+			</div>
 		</section>
 	);
 };
